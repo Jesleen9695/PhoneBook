90ad378548011278b2e150f48cca3a669a0ee62c
Jesleen9695 <57336673+Jesleen9695@users.noreply.github.com>
2021-08-05 23:25:46 -0500

    Delete PhoneBook/Contact directory

--- PhoneBook/Contact/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . import models
-
-class AddContact(forms.ModelForm):
-    class Meta:
-        model = models.Contacts
-        fields = ['first_name', 'last_name', 'phone', 'email_addr', 'street_addr']
